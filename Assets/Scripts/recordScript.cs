108dfa898ea0c665c99a3f160ca26c358d306d88
hailsong <hailsong@yonsei.ac.kr>
2023-02-24 16:33:18 +0900

    developing save/load scene

--- Assets/Scripts/recordScript.cs
+++ Assets/Scripts/recordScript.cs
@@ -1,19 +1,153 @@
-﻿using UnityEngine;
+﻿using Microsoft.Azure.Kinect.BodyTracking;
 using Microsoft.Azure.Kinect.Sensor;
+using UnityEngine;
 
 using System.Collections;
 using System.Collections.Generic;
-
+using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
+
 using System.Linq;
+
 
 [System.Serializable]
 class SaveData
 {
-    // public List<Vector3[]> verticesSave;
-    public List<Color32[]> colorSave;
+
+    [Serializable]
+    public class SerializableVector3
+    {
+        public float[] x;
+        public float[] y;
+        public float[] z;
+
+        public SerializableVector3(Vector3[] vectors)
+        {
+            x = vectors.Select(v => v.x).ToArray();
+            y = vectors.Select(v => v.y).ToArray();
+            z = vectors.Select(v => v.z).ToArray();
+        }
+
+        public SerializableVector3(Vector3 vector3)
+        {
+            Vector3 = vector3;
+        }
+
+        public Vector3 Vector3 { get; }
+
+        public Vector3[] ToVector3Array()
+        {
+            int length = x.Length;
+            Vector3[] vectors = new Vector3[length];
+            for (int i = 0; i < length; i++)
+            {
+                vectors[i] = new Vector3(x[i], y[i], z[i]);
+            }
+            return vectors;
+        }
+
+    }
+
+
+    [Serializable]
+    public class SerializableColor32
+    {
+        public byte[] r;
+        public byte[] g;
+        public byte[] b;
+        public byte[] a;
+
+        public SerializableColor32(Color32[] colors)
+        {
+            r = colors.Select(c => c.r).ToArray();
+            g = colors.Select(c => c.g).ToArray();
+            b = colors.Select(c => c.b).ToArray();
+            a = colors.Select(c => c.a).ToArray();
+        }
+
+        public Color32[] ToColor32Array()
+        {
+            int length = r.Length;
+            Color32[] colors = new Color32[length];
+            for (int i = 0; i < length; i++)
+            {
+                colors[i] = new Color32(r[i], g[i], b[i], a[i]);
+            }
+            return colors;
+        }
+    }
+
+    //[Serializable]
+    //public class SerializableBody
+    //{
+    //    public SerializableVector3[] JointPositions3D;
+    //    public SerializableQuaternion[] JointRotations;
+
+    //    public SerializableBody(Body body)
+    //    {
+    //        JointPositions3D = body.JointPositions3D.Select(v => new SerializableVector3(ConvertToUnityVector3(v))).ToArray();
+    //        JointRotations = body.JointRotations.Select(q => new SerializableQuaternion(ConvertToUnityQuaternion(q))).ToArray();
+    //    }
+
+    //    public Body ToBody()
+    //    {
+    //        Body body = new Body();
+    //        for (int i = 0; i < JointPositions3D.Length; i++)
+    //        {
+    //            body.JointPositions3D[i] = ConvertToNumericsVector3(JointPositions3D[i].ToVector3Array());
+    //            body.JointRotations[i] = ConvertToNumericsQuaternion(JointRotations[i].ToQuaternion());
+    //        }
+    //        return body;
+    //    }
+
+    //    private static UnityEngine.Vector3 ConvertToUnityVector3(System.Numerics.Vector3 v)
+    //    {
+    //        return new UnityEngine.Vector3(v.X, v.Y, v.Z);
+    //    }
+
+    //    private static System.Numerics.Vector3 ConvertToNumericsVector3(UnityEngine.Vector3 v)
+    //    {
+    //        return new System.Numerics.Vector3(v.x, v.y, v.z);
+    //    }
+
+    //    private static UnityEngine.Quaternion ConvertToUnityQuaternion(System.Numerics.Quaternion q)
+    //    {
+    //        return new UnityEngine.Quaternion(q.X, q.Y, q.Z, q.W);
+    //    }
+
+    //    private static System.Numerics.Quaternion ConvertToNumericsQuaternion(UnityEngine.Quaternion q)
+    //    {
+    //        return new System.Numerics.Quaternion(q.x, q.y, q.z, q.w);
+    //    }
+    //}
+
+    //[Serializable]
+    //public class SerializableQuaternion
+    //{
+    //    public float x;
+    //    public float y;
+    //    public float z;
+    //    public float w;
+
+    //    public SerializableQuaternion(Quaternion quaternion)
+    //    {
+    //        x = quaternion.x;
+    //        y = quaternion.y;
+    //        z = quaternion.z;
+    //        w = quaternion.w;
+    //    }
+
+    //    public Quaternion ToQuaternion()
+    //    {
+    //        return new Quaternion(x, y, z, w);
+    //    }
+    //}
+
+
+    public List<SerializableVector3> verticesSave;
+    public List<SerializableColor32> colorSave;
     public List<int[]> indicesSave;
     // public List<Body> bodySave;
 }
@@ -74,8 +208,8 @@ public class recordScript : MonoBehaviour
     public int updateNumber = 0;
     //public Texture2D tex = new Texture2D(1024, 1024, TextureFormat.RGB24, false);
 
-    List<Vector3[]> verticesList = new List<Vector3[]>();
-    List<Color32[]> colorList = new List<Color32[]>();
+    List<SaveData.SerializableVector3> verticesList = new List<SaveData.SerializableVector3>();
+    List<SaveData.SerializableColor32> colorList = new List<SaveData.SerializableColor32>();
     List<int[]> indicesList = new List<int[]>();
     List<Body> bodyList = new List<Body>();
 
@@ -127,7 +261,7 @@ public class recordScript : MonoBehaviour
 
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (m_skeletalTrackingProvider.IsRunning)
         {
@@ -159,14 +293,17 @@ public class recordScript : MonoBehaviour
                 if (nowRecording)
                 {
                     // verticesList.Add(vertices);
-                    colorList.Add(colors);
+                    SaveData.SerializableVector3 serializableVector = new SaveData.SerializableVector3(vertices);
+                    SaveData.SerializableColor32 serializableColor = new SaveData.SerializableColor32(colors);
+                    verticesList.Add(serializableVector);
+                    colorList.Add(serializableColor);
                     indicesList.Add(indices);
                     // bodyList.Add(body);
-
+                    Debug.Log($"{verticesList.Count}, {colorList.Count}, {indicesList.Count}, {bodyList.Count}");
                 }
                 
 
-                Debug.Log($"{verticesList.Count}, {colorList.Count}, {indicesList.Count}, {bodyList.Count}");
+                
 
                 if (verticesList.Count > 500)
                 {
@@ -198,7 +335,7 @@ public class recordScript : MonoBehaviour
         BinaryFormatter bf = new BinaryFormatter();
         FileStream fs = new FileStream(fileName, FileMode.Create);
 
-        // mySaveData.verticesSave = verticesList;
+        mySaveData.verticesSave = verticesList;
         mySaveData.colorSave = colorList;
         mySaveData.indicesSave = indicesList;
         // mySaveData.bodySave = bodyList;
@@ -225,5 +362,12 @@ public class recordScript : MonoBehaviour
 
 
 
+    }
+    void OnApplicationQuit()
+    {
+        if (m_skeletalTrackingProvider != null)
+        {
+            m_skeletalTrackingProvider.Dispose();
+        }
     }
 }
